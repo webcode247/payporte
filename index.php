9989318473bba2386d1d1e09d30bc47cb09e624d
webcode247 <webcode247@gmail.com>
2017-06-11 18:04:56 +0100

    first page

---
+++ index.php
@@ -0,0 +1,338 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Pay</title>
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/font-awesome.css">
+
+</head>
+
+<body>
+<div class="wrap">
+<!--menu starts-->
+<div class="top_bar">
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+ <div class="top_bar_inner">
+ 	<div class="icon_bar">
+ 		<div class="col-sm navbar-nav navbar-links">
+          	<span>
+          		<a href="" ><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+			<a href="" class="badge">0</a></span>
+          	<span>
+          		<a href=""><i class="fa fa-heart-o" aria-hidden="true"></i></a> 
+          		<a href="" class="badge badge-2">0</a></span>
+          	<span><a href="">Login / Sign Up</a></span>
+			</div>
+ 	</div>
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><img src="image/logo.gif" alt="Logo"></a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Stores</a></li>
+        <li><a href="#">Become a Vendor</a></li>
+        <li><a href="#">FAQs</a>
+       
+        </li>
+      </ul>
+     
+       
+      
+    </div><!-- /.navbar-collapse -->
+ </div>
+  </div><!-- /.container-fluid -->
+</nav>
+</div><!--top_bar-->
+<!--menu end-->
+
+<!--=======searchbar starts==========-->
+<div class="top_bar_inner">	
+<div class="col-sm-12">
+<div class="row">
+<form>
+<div class="form-group">
+<input type="text" class="form-control" placeholder="Type something here">
+<span class="navbar-right">
+	<i class="fa fa-search" aria-hidden="true"></i>
+</span> 
+
+</div>
+</form>
+</div>    
+</div><!--12-->
+</div>
+<!--==========searchbar ends======-->
+
+<!--==========slider starts=============-->
+<div class="slider">
+<div class="carousel slide" data-ride="carousel" id="carousel">
+ <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      	<img src="image/slide.jpg" alt="slide1" width="100%">
+      	<div class="carousel-caption">
+        <h2><p><span>Top brands. Bulk purchase.</span></p>
+        <p><span class="span2">One megastore.</span></p></h2>
+         
+         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br> sed do eiusmod. Ut enim ad minim veniam, quis nostrud<br> exercitation.</p>
+         <button>LEARN MORE</button>
+		</div>	
+    </div>
+</div>
+  <!-- Controls -->
+
+</div>
+</div>
+<!--=================slider ends==============-->
+<div class="info_area">
+<div class="top_bar_inner">
+<div class="inner_wrapper">
+
+<div class="info_head">
+	<div class="page-header">Are you a fashion retailer?</div>
+<p>Top reasons you should have PayPorte produced fashion products in your store</p>
+<div class="divider"></div>
+</div><!--info_head-->
+
+<div class="col-sm-12 info_body">
+<div class="row">
+<div class="col-sm-4">
+	<div class="media">
+		  <div class="media-left col-sm-3">
+  <div class="row">
+    <a href="#">
+      <img class="media-object" src="image/quality.png"  alt="Premium quality">
+    </a>
+  </div></div>
+    <div class="media-body col-sm-9">
+    <h4 class="media-heading">Premium Quality at <br>most affordable prices</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  </div>
+	</div><!--media-->
+</div>
+<div class="col-sm-4">
+	<div class="media">
+		  <div class="media-left col-sm-3">
+  <div class="row">
+    <a href="#">
+      <img class="media-object" src="image/design.png"  alt="Trendy design">
+    </a>
+  </div></div>
+    <div class="media-body col-sm-9">
+<h4 class="media-heading">Trendy designs crafted <br>by international<br>designers</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  </div>
+	</div><!--media-->
+</div>
+<div class="col-sm-4">
+	<div class="media">
+		  <div class="media-left col-sm-3">
+  <div class="row">
+    <a href="#">
+      <img class="media-object" src="image/facility.png"  alt="Production facilities">
+    </a>
+  </div></div>
+    <div class="media-body col-sm-9">
+<h4 class="media-heading">Production facilities and  <br>operations in China, US,<br>UK and Nigeria </h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  </div>
+	</div><!--media-->
+</div>
+</div>	
+
+<div class="row">
+<div class="col-sm-4">
+	<div class="media">
+		  <div class="media-left col-sm-3">
+  <div class="row">
+    <a href="#">
+      <img class="media-object" src="image/exclusive.png"  alt="Exclusive design">
+    </a>
+  </div></div>
+    <div class="media-body col-sm-9">
+    <h4 class="media-heading">Exclusive designs  <br>inspired by international <br>market trends</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  </div>
+	</div><!--media-->
+</div>
+<div class="col-sm-4">
+	<div class="media">
+		  <div class="media-left col-sm-3">
+  <div class="row">
+    <a href="#">
+      <img class="media-object" src="image/quick.png" alt="Quick response">
+    </a>
+  </div></div>
+    <div class="media-body col-sm-9">
+    <h4 class="media-heading">Quick and flexible <br>responses to bespoke <br>requests</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  </div>
+	</div><!--media-->
+</div>
+<div class="col-sm-4">
+	<div class="media">
+		  <div class="media-left col-sm-3">
+  <div class="row">
+    <a href="#">
+      <img class="media-object" src="image/delivery.png"  alt="Affordable deliveries">
+    </a>
+  </div></div>
+    <div class="media-body col-sm-9">
+    <h4 class="media-heading">Affordable deliveries in as  <br>short as 3 days</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  </div>
+	</div><!--media-->
+</div>
+</div>
+</div><!--info_body-->
+<div class="form_area">
+	
+<div class="info_head">
+	<div class="divider" style="margin-bottom:3%;"></div>
+<div class="page-header" style=" padding-top:0;margin:0;font-size:130%">Too good to be true? Don’t take our word for it.</div>
+<p>Send us a message and we’ll get back to you very soon.</p>
+</div><!--info_head-->
+
+<div class="col-sm-5 col-sm-offset-4">
+	<form action="includes/process.php" method="post" class="ajax">
+	<div class="col-sm-12"><div class="row">
+		<div class="form-group">
+			<label for="exampleInputName">Name</label>
+    <input type="text" class="form-control" id="" name="name" required>
+ </div></div>
+	</div>
+
+<div class="row">
+	<div class="col-sm-6">
+		<div class="form-group">
+			<label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="" name="email" required>
+ </div>
+	</div>
+	<div class="col-sm-6">
+		<div class="form-group"><label for="Phone Number">Phone Number</label>
+    <input type="number" class="form-control" id="" name="telephone" required>
+ </div></div>
+	</div>
+		<div class="col-sm-12"><div class="row">
+		<div class="form-group">
+			<label for="exampletextarea">Your Message</label>
+    <textarea  class="form-control"  rows="6" name="message" required></textarea>
+ </div>
+	</div></div>
+	<button class="btn btn-danger" id="">Submit</button>
+</form>
+</div>
+</div><!--green-->
+</div><!--inner_wrapper-->
+</div><!--top_bar_inner-->
+
+<div class="footer_area">
+	<div class="upper-footer">
+	<div class="top_bar_inner">
+	<div class="inner_wrapper">
+		<div class="container">
+						<div class="row">
+			<div class="upper_footer">
+			<div class="col-sm-4">
+
+				<div class="page-header story">our story</div>
+				<p>Payporte is an ecommerce with headquaters in Lagos,</p><p> Nigeria. It boasts of being the face of Africa's online market</p>
+			</div>
+			<div class="col-sm-8 right">
+				<div class="col-sm-3">
+					<div class="page-header">company</div>
+					<ul>
+						<li><a href="">about us</a></li>
+						<li><a href="">careers</a></li>
+						<li><a href="">partnership</a></li>
+						<li><a href="">contact us</a></li>
+					</ul></div>
+				<div class="col-sm-3">
+					<div class="page-header">how it works</div>
+					<ul>
+						<li><a href="">site tours</a></li>
+						<li><a href="">marketplace</a></li>
+						<li><a href="">FAQs</a></li>
+						<li><a href="">shopping tips</a></li>
+					</ul></div>
+				<div class="col-sm-3">
+					<div class="page-header">stores</div>
+					<ul>
+						<li><a href="">the 1k stores</a></li>
+						<li><a href="">happy feet</a></li>
+						<li><a href="">40 percent off</a></li>
+						<li><a href="">qupid wednesday</a></li>
+					</ul></div>
+				<div class="col-sm-3">
+					<div class="row">
+					<div class="page-header">service & support</div>
+					<ul>
+						<li><a href="">privacy policy</a></li>
+						<li><a href="">orders & returns</a></li>
+						<li><a href="">shipping policy</a></li>
+						<li><a href="">terms & conditions</a></li>
+					</ul></div></div>
+			</div>
+			</div> <!--upper_footer-->
+		</div><!--container-->
+
+	</div>
+	</div></div><!--upper-footer-->
+	</div>
+	<div class="mid_footer">
+		<div class="top_bar_inner">
+	<div class="inner_wrapper">
+				<div class="col-sm-4 col-sm-push-8">
+				<ul>
+					<li><a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+					<li><a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+					<li><a href=""><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+					<li><a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+					<li><a href=""><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+				</ul>
+				</div>
+				<div class="col-sm-8 col-sm-pull-4">
+					<ul>
+						<li><img src="image/paypal.png" alt=""></li>
+						<li><img src="image/visa.png" alt=""></li>
+						<li><img src="image/maestro.png" alt=""></li>
+						<li><img src="image/mastercard.png" alt=""></li>
+					</ul>
+				</div>
+	</div>
+	</div>
+	</div><!--mid-footer-->
+
+<div class="bottom_footer">
+	<div class="top_bar_inner">
+	<div class="inner_wrapper">
+		
+		<div class="col-sm-4 col-sm-push-8"><p><a href="mailto:info@payporte.com">info@payporte.com</a> | 0808 PAYPORTE(0800)726-76783</p></div>
+			<div class="col-sm-8 col-sm-pull-4"><p>copyright&copy; 2016. Payporte.com. All Rights Reserved</p></div>
+	</div>
+	</div>
+</div><!--bottom_footer-->	
+</div><!--info_area-->
+
+
+</div><!--wrap-->
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
